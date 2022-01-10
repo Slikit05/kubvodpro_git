aa1a2f5bb4331062697466bd663e3003f61358c5
Slikit05 <slikit@mail.ru>
2022-01-10 14:56:02 +0300

    10.01.2022

--- build/js/main.js
+++ build/js/main.js
@@ -1,57 +1,57 @@
 (function () {
-	let changeScrollNumber = 0;
-	const header = document.querySelector('header').scrollHeight;
-	const firstScreen = document.querySelector('.first-screen').scrollHeight;
-	const firstScroll = header + firstScreen;
+	// let changeScrollNumber = 0;
+	// const header = document.querySelector('header').scrollHeight;
+	// const firstScreen = document.querySelector('.first-screen').scrollHeight;
+	// const firstScroll = header + firstScreen;
 
-	if (document.documentElement.clientWidth > 1023) {
-		if (window.pageYOffset >= firstScroll) {
-			document.querySelector('body').classList.remove('hiden');
-			changeScrollNumber = 1;
-		} else if (window.pageYOffset > 0) {
-			document.querySelector('body').classList.remove('hiden');
-			changeScrollNumber = 1;
-		} else {
-			document.querySelector('body').classList.add('hiden');
-		}
-	}	
+	// if (document.documentElement.clientWidth > 1023) {
+	// 	if (window.pageYOffset >= firstScroll) {
+	// 		document.querySelector('body').classList.remove('hiden');
+	// 		changeScrollNumber = 1;
+	// 	} else if (window.pageYOffset > 0) {
+	// 		document.querySelector('body').classList.remove('hiden');
+	// 		changeScrollNumber = 1;
+	// 	} else {
+	// 		document.querySelector('body').classList.add('hiden');
+	// 	}
+	// }	
 
-	document.querySelectorAll('.arrow-scroll').forEach(link => {
+	// document.querySelectorAll('.arrow-scroll').forEach(link => {
 
-		link.addEventListener('click', function (e) {
+	// 	link.addEventListener('click', function (e) {
 
 
-			let href = this.getAttribute('href');
+	// 		let href = this.getAttribute('href');
 
-			const scrollTarget = document.querySelector(href);
+	// 		const scrollTarget = document.querySelector(href);
 
-			const topOffset = document.querySelector('.scroll-header').scrollHeight;
-			const elementPosition = scrollTarget.getBoundingClientRect().top;
-			const offsetPosition = elementPosition - topOffset;
+	// 		const topOffset = document.querySelector('.scroll-header').scrollHeight;
+	// 		const elementPosition = scrollTarget.getBoundingClientRect().top;
+	// 		const offsetPosition = elementPosition - topOffset;
 
-			window.scrollBy({
-				top: firstScroll - 210,
-				behavior: 'smooth'
-			});
-			document.querySelector('body').classList.remove('hiden');
-			changeScrollNumber = 1;
-			e.preventDefault();
-		});
-	});
+	// 		window.scrollBy({
+	// 			top: firstScroll - 210,
+	// 			behavior: 'smooth'
+	// 		});
+	// 		document.querySelector('body').classList.remove('hiden');
+	// 		changeScrollNumber = 1;
+	// 		e.preventDefault();
+	// 	});
+	// });
 
-	if (document.documentElement.clientWidth > 1023) {
-		window.addEventListener("wheel", event => {
-			if (window.pageYOffset <= firstScroll && changeScrollNumber === 0) {
-				window.scrollBy({
-					top: firstScroll - 210,
-					behavior: 'smooth'
-				});
-				changeScrollNumber = 1;
-			} else if (changeScrollNumber === 1) {
-				document.querySelector('body').classList.remove('hiden');
-			};
-		});
-	}
+	// if (document.documentElement.clientWidth > 1023) {
+	// 	window.addEventListener("wheel", event => {
+	// 		if (window.pageYOffset <= firstScroll && changeScrollNumber === 0) {
+	// 			window.scrollBy({
+	// 				top: firstScroll - 210,
+	// 				behavior: 'smooth'
+	// 			});
+	// 			changeScrollNumber = 1;
+	// 		} else if (changeScrollNumber === 1) {
+	// 			document.querySelector('body').classList.remove('hiden');
+	// 		};
+	// 	});
+	// }
 
 	
 })();
@@ -98,9 +98,21 @@
 window.addEventListener('click', function (event) {
 	if (event.target.classList.contains('search-icon')) {
 		if (event.target.closest('.wrap-search').querySelector('.search-field').classList.contains('search-field--open')) {
-			event.target.closest('.wrap-search').querySelector('.search-field').classList.remove('search-field--open');
+			if (event.target.closest('.wrap-search').nextElementSibling) {
+				event.target.closest('.wrap-search').nextElementSibling.classList.remove('hide-elem');
+			}
+			if (event.target.closest('.page-header__wrapper')) {
+				event.target.closest('.page-header__wrapper').querySelector('.logo__flex').classList.remove('hide-text');
+			}
+			event.target.closest('.wrap-search').querySelector('.search-field').classList.remove('search-field--open');			
 			event.target.classList.remove('search-icon--open')
 		} else {
+			if (event.target.closest('.wrap-search').nextElementSibling) {
+				event.target.closest('.wrap-search').nextElementSibling.classList.add('hide-elem');
+			}
+			if (event.target.closest('.page-header__wrapper')) {
+				event.target.closest('.page-header__wrapper').querySelector('.logo__flex').classList.add('hide-text');
+			}
 			event.target.closest('.wrap-search').querySelector('.search-field').classList.add('search-field--open');
 			event.target.classList.add('search-icon--open')
 		}
@@ -108,5 +120,70 @@ window.addEventListener('click', function (event) {
 });
 
 // Поле поиска -- конец
+
+// Мобильное меню
+
+(function () {
+  const btnBurger = document.querySelector(".burger"),
+    mobileMenu = document.querySelector("[data-mobile-menu]");
+
+  btnBurger.addEventListener("click", openMobileMenu);
+
+  function openMobileMenu(event) {
+    if (mobileMenu.classList.contains("mob-menu--open")) {
+      mobileMenu.classList.remove("mob-menu--open");
+			btnBurger.classList.remove("burger--open")
+      document.querySelector("html").classList.remove("hiden");
+    } else {
+      mobileMenu.classList.add("mob-menu--open");
+			btnBurger.classList.add("burger--open")
+      document.querySelector("html").classList.add("hiden");
+    }
+  }
+})();
+
+// Мобильное меню - конец
+
+// акордионы
+
+class Accordion {
+  constructor(selector) {
+    this.selector = selector;
+  }
+
+  createAccordion(selector) {
+    const accordionList = document.querySelectorAll(this.selector);
+    for (let element of accordionList) {
+      element.addEventListener("click", function () {
+        this.classList.toggle("open");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else if (panel.closest(".open-accordion")) {
+          // console.log(this);
+          let parentAccordion = this.closest("[data-accordion-content]");
+          parentAccordion.style.maxHeight = parentAccordion.scrollHeight + panel.scrollHeight + "px";
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+        let parentElem = this.parentNode;
+        if (this.classList.contains("open")) {
+          parentElem.classList.add("open-accordion");
+        } else {
+          parentElem.classList.remove("open-accordion");
+        }
+      });
+    }
+  }
+}
+
+const accMobMenu = new Accordion(".mob-menu-list__up--drop");
+const accFooter = new Accordion(".footer-nav__wrap-up--drop");
+
+accMobMenu.createAccordion();
+accFooter.createAccordion();
+
+// акордионы  - конец
 
 Inputmask("+7 (999) 999-99-99").mask('[type="tel"]');
